40cf9c6bc71fefdcbd071cd4efb4594df7c589a7
JTong <eldertong@gmail.com>
2016-11-29 13:31:27 -0500

    splitting input word to an array of strings of each letter

--- app.rb
+++ app.rb
@@ -2,7 +2,12 @@ class Hangman
     attr_accessor :empty_blanks_for_word
 
     def initialize(num_of_letters)
-        @empty_blanks_for_word = Array.new(num_of_letters, " ")
+        @empty_blanks_for_word = Array.new(num_of_letters.to_i, " ")
     end
+
+    def input_secret_word(input_word)
+        game_word = input_word.split("")
+    end
+        
 
 end
--- apptest.rb
+++ apptest.rb
@@ -5,10 +5,12 @@ class TestApp < Minitest::Test
 
     def test_input_word_returns_array_with_number_of_letters_empty_stringed
         guess_word = Hangman.new(4)
-        assert_equal([" "," "," "," "], guess_word.word)
+        assert_equal([" "," "," "," "], guess_word.empty_blanks_for_word)
     end
 
-    def test_house_is_the_game_word
-        game_word =  
+    def test_input_word_returns_array_with_each_letter_as_string
+        input_word = "House"
+        assert_equal(["H", "o", "u", "s", "e"], input_word.game_word)
+    end
 
 end
