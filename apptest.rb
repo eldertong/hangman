32f8f4763f3d0247b605d339fe94e04c0010c661
JTong <eldertong@gmail.com>
2016-11-29 12:58:17 -0500

    initial commit

---
+++ apptest.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require_relative "app.rb"
+
+class TestApp < Minitest::Test
+
+    def test_input_word_number_of_letters
+        game_word = Hangman.new(4)
+        assert_equal([" "," "," "," "], game_word.word)
+    end
+
+    def test_
+
+end
