d0e1de2c16e0b75160c99dd3356162078732bdf5
JTong <eldertong@gmail.com>
2016-11-29 15:42:27 -0500

    adam helped me figure out the includes

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ class Hangman
     attr_accessor :word
 
     def initialize(word)
-        @word = word
+        @word = word.downcase!
     end
 
     def input_secret_word
@@ -12,6 +12,14 @@ class Hangman
     def secret_word_displayed
         number_of_blanks = @word.length
         blank_word = Array.new(number_of_blanks, "")
+    end
+
+    def word_include?(letter_guess)
+        if @word.include? letter_guess
+            true
+        else
+            false
+        end
     end
 
         
--- apptest.rb
+++ apptest.rb
@@ -5,12 +5,12 @@ class TestApp < Minitest::Test
 
     def test_input_word_returns_word
         game_word = Hangman.new("House")
-        assert_equal("House", game_word.word)
+        assert_equal("house", game_word.word)
     end
 
     def test_input_word_is_split_to_array_of_strings
         game_word = Hangman.new("House")
-        assert_equal(["H", "o", "u", "s", "e"], game_word.input_secret_word)
+        assert_equal(["h", "o", "u", "s", "e"], game_word.input_secret_word)
     end
 
     def test_word_returns_empty_array_same_number_of_strings
@@ -18,6 +18,17 @@ class TestApp < Minitest::Test
         assert_equal(["", "", "", "", ""], game_word.secret_word_displayed)
     end
 
+    def test_o_is_a_correct_guess
+        game_word = Hangman.new("House")
+        letter_guess = "o"
+        assert_equal(true, game_word.word_include?(letter_guess))
+    end
+
+    def test_r_is_a_false_guess
+        game_word = Hangman.new("House")
+        letter_guess = "r"
+        assert_equal(false, game_word.word_include?(letter_guess))
+    end
 
     # def test_input_word_returns_array_with_each_letter_as_string
     #     game = Hangman.new()
