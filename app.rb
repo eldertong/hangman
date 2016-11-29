cd65ff5fc38dc4f6267a87260d48b64b4281d6ad
JTong <eldertong@gmail.com>
2016-11-29 13:13:11 -0500

    changing variable  names

--- app.rb
+++ app.rb
@@ -1,8 +1,8 @@
 class Hangman
-    attr_accessor :word
+    attr_accessor :empty_blanks_for_word
 
     def initialize(num_of_letters)
-        @word = Array.new(num_of_letters, " ")
+        @empty_blanks_for_word = Array.new(num_of_letters, " ")
     end
 
 end
--- apptest.rb
+++ apptest.rb
@@ -3,11 +3,12 @@ require_relative "app.rb"
 
 class TestApp < Minitest::Test
 
-    def test_input_word_number_of_letters
-        game_word = Hangman.new(4)
-        assert_equal([" "," "," "," "], game_word.word)
+    def test_input_word_returns_array_with_number_of_letters_empty_stringed
+        guess_word = Hangman.new(4)
+        assert_equal([" "," "," "," "], guess_word.word)
     end
 
-    def test_
+    def test_house_is_the_game_word
+        game_word =  
 
 end
