e88e731a31af64923116ca81177ca7d06db381f6
JTong <eldertong@gmail.com>
2016-11-29 14:26:06 -0500

    secret word displaying blanks

--- app.rb
+++ app.rb
@@ -6,9 +6,13 @@ class Hangman
     end
 
     def input_secret_word
-        split_word = @word.split("")
+        @split_word = @word.split("")
     end
 
+    def secret_word_displayed
+        number_of_blanks = @word.length
+        blank_word = Array.new(number_of_blanks, "")
+    end
 
         
 
--- apptest.rb
+++ apptest.rb
@@ -13,6 +13,10 @@ class TestApp < Minitest::Test
         assert_equal(["H", "o", "u", "s", "e"], game_word.input_secret_word)
     end
 
+    def test_word_returns_empty_array_same_number_of_strings
+        game_word = Hangman.new("House")
+        assert_equal(["", "", "", "", ""], game_word.secret_word_displayed)
+    end
 
 
     # def test_input_word_returns_array_with_each_letter_as_string
