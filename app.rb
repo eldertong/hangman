27f4c7d8f802a2bf7954eb8748b86fe2d76d4066
JTong <eldertong@gmail.com>
2016-12-01 10:29:52 -0500

    unexpected end?

--- app.rb
+++ app.rb
@@ -2,6 +2,15 @@ require 'sinatra'
 require 'rubygems'
 require_relative 'hangman_class.rb'
 
-get '/' do
-    erb :home
-end
+enable :sessions
+
+
+
+    get '/' do
+        erb :home
+    end
+
+    get '/enter_word'
+        erb :enter_word, :locals => { word => :word }
+        redirect '/play_game'
+    end
